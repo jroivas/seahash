e789e8eba41b3c89ad68114d0a7bad026e4c937b
Jouni Roivas <jroivas@iki.fi>
2017-03-01 19:05:07 +0200

    Optimize reading

--- seahash.cpp
+++ seahash.cpp
@@ -5,34 +5,18 @@
 
 #include "seastate.hh"
 
-uint64_t readData(std::ifstream &s, uint64_t &len)
+uint64_t readData(std::ifstream &s, uint64_t &i)
 {
     uint64_t res = 0;
-    uint64_t i = 0;
+
     while (!s.eof() && s.good() && i < sizeof(uint64_t)) {
         char c = s.get();
         if (!s.eof()) {
-            res <<= 8;
-            ++len;
-            res |= uint64_t(c);
+            res |= uint64_t(c) << (i * 8);
             ++i;
         }
     }
-    // Padding
-    while (i < sizeof(uint64_t)) {
-        res <<= 8;
-        ++i;
-    }
-    // Reverse since read on "wrong" order from file
-    uint64_t res2 = 0;
-    i = 0;
-    while (i < sizeof(uint64_t)) {
-        res2 <<= 8;
-        res2 |= (res & 0xFF);
-        res >>= 8;
-        ++i;
-    }
-    return res2;
+    return res;
 }
 
 int main(int argc, char **argv)
